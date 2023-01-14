ccb91837650dbbdeccd9a120a05d72fc4c1aec2b
nbaglietto <37049113+nbaglietto@users.noreply.github.com>
2023-01-14 13:20:17 -0300

    primera subida

---
+++ public/js/resume.js
@@ -0,0 +1,45 @@
+
+function zoomImages(className) {
+  $(`.${className}`).unbind("click").on("click", function() {
+    $("#div-zoom-area").css("display", "block");
+    $("#img-zoom-area-content").attr("src", $(this)[0].src);
+
+    $("#div-zoom-area").on("click", function() {
+      $(this).css("display", "none");
+    });
+  });
+}
+
+(function($) {
+  "use strict"; // Inicio de uso estricto
+
+  // Desplazamiento suave usando la aceleración de jQuery
+  $("a.js-scroll-trigger[href*='#']:not([href='#'])").click(function() {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate({
+          scrollTop: (target.offset().top)
+        }, 500, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Cierra el menú receptivo cuando se hace clic en un enlace de activación de desplazamiento
+  $(".js-scroll-trigger").click(function() {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Active scrollspy para agregar una clase activa a los elementos de la barra de navegación en el desplazamiento
+  $("body").scrollspy({
+    target: "#sideNav"
+  });
+  
+
+
+  // Zoom de imágenes que tienen nombre de clase: img-zoomable
+  zoomImages("img-zoomable");
+
+})(jQuery); //Fin de uso estricto
